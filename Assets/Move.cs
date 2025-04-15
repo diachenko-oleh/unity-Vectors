bdf9301aafb3cef23d75268a3d55e9b4b9785243
diachenko-oleh <diachenko.oleh1122@vu.cdu.edu.ua>
2025-04-15 16:28:25 +0300

    initial commit

---
+++ Assets/Move.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public GameObject goal;
+    Vector3 direction;
+    public float speed = 2f;
+
+    void Start() {
+
+    }
+
+    private void LateUpdate()
+    {
+        direction = goal.transform.position - transform.position;
+        transform.LookAt(goal.transform.position);
+        if (direction.magnitude > 2)
+        {
+            Vector3 velocity = direction.normalized * speed * Time.deltaTime;
+            this.transform.position = transform.position + velocity;
+
+        }
+    }
+}
